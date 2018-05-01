e07ddd46f9b2134ef1905514cd1047b26dd89fb2
Jinyoung Park <iamjy1005@gmail.com>
2018-05-02 07:01:09 +0900

    [OSAL] Modify build script

--- user-library/src/opsrc-osal/jyp-build.sh
+++ user-library/src/opsrc-osal/jyp-build.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
+TARGET_ARCH="$1"
+TOOLCHAIN_FILE="cmake-rule/toolchain.arm.cmake"
+SYSTEM_OSTYPE=posix
+SYSTEM_BSTYPE=pc-linux
+INCLUDEDIR="/home/iamjy1005/work/lab/practice/user-library/src/opsrc-osal/src/bsp/${SYSTEM_BSTYPE}/config"
+
+SRC_DIR=${PWD}
+
 if [ "$1" = "clean" ]; then
 	rm -rf CMakeCache.txt
 	rm -rf CMakeFiles
 	exit 0
 fi
 
-cmake -DCMAKE_TOOLCHAIN_FILE=cmake-rule/toolchain.arm.cmake -DOSAL_SYSTEM_OSTYPE=posix -DOSAL_SYSTEM_BSPTYPE=pc-linux -DOSAL_INCLUDEDIR=/home/iamjy1005/work/lab/practice/user-library/src/opsrc-osal/src/bsp/pc-linux/config /home/iamjy1005/work/lab/practice/user-library/src/opsrc-osal
+if [ "${TARGET_ARCH}" = "arm" ]; then
+	cmake -DCMAKE_TOOLCHAIN_FILE=${TOOLCHAIN_FILE} -DOSAL_SYSTEM_OSTYPE=${SYSTEM_OSTYPE} -DOSAL_SYSTEM_BSPTYPE=${SYSTEM_BSTYPE} -DOSAL_INCLUDEDIR=${INCLUDEDIR} ${SRC_DIR}
+
+else
+	cmake -DOSAL_SYSTEM_OSTYPE=${SYSTEM_OSTYPE} -DOSAL_SYSTEM_BSPTYPE=${SYSTEM_BSTYPE} -DOSAL_INCLUDEDIR=${INCLUDEDIR} ${SRC_DIR}
+	
+fi
